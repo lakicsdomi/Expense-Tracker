f33ebc6499abc0a631b833544d0e3fa73c891bb3
Domonkos Lakics <qq7oak@INF.ELTE.HU>
2025-04-29 18:05:23 +0200

    Added methods to `IDateUtils` and `IFileHandler`

--- src/main/java/com/github/lakicsdomi/expensetracker/persistence/IFileHandler.java
+++ src/main/java/com/github/lakicsdomi/expensetracker/persistence/IFileHandler.java
@@ -1,4 +1,11 @@
 package com.github.lakicsdomi.expensetracker.persistence;
 
+import com.github.lakicsdomi.expensetracker.model.Expense;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface IFileHandler {
+    void saveExpensesToFile(List<Expense> expenses, String filePath) throws IOException;
+    List<Expense> loadExpensesFromFile(String filePath) throws IOException, ClassNotFoundException;
 }
--- src/main/java/com/github/lakicsdomi/expensetracker/service/IExpenseManager.java
+++ src/main/java/com/github/lakicsdomi/expensetracker/service/IExpenseManager.java
@@ -1,4 +1,19 @@
 package com.github.lakicsdomi.expensetracker.service;
 
+import com.github.lakicsdomi.expensetracker.model.Expense;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface IExpenseManager {
+    void addExpense(Expense expense);
+    void deleteExpense(int id);
+    double calculateTotalByCategory(String category);
+    Expense getExpenseById(int id);
+    Expense getExpenseByCategory(String category);
+    List<Expense> getExpenses();
+    List<Expense> getExpensesByCategory(String category);
+    List<Expense> getExpensesByCategoryAndDate(String category, LocalDate date);
+    List<Expense> getExpensesByDateRange(LocalDate start, LocalDate end);
+
 }
